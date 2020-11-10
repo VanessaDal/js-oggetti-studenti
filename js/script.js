6f5ae071d6ad9f9d340d838bcbce446a4afa9ccc
Vanessa D'Alessandro <leavesthatfall@gmail.com>
2020-11-10 13:16:49 +0100

    es2

--- js/script.js
+++ js/script.js
@@ -1,7 +1,37 @@
 // Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
 
-var studente={'nome':"pippo", 'cognome':'birillo', 'eta':30}
+var studente=
+    {
+    'nome':"pippo", 
+    'cognome':'birillo', 
+    'eta':30
+    }
 
 for (var key in studente){
     console.log(key + " = " + studente[key])
 }
+
+//Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+var arrayStudenti = [
+    {
+      'nome' : 'Vanessa',
+      'cognome' : "D'Alessandro",
+      'eta' : '32'
+    },
+    {
+      'nome' : 'Billy',
+      'cognome' : 'Ballo',
+      'eta' : '36'
+    },
+    {
+      'nome' : 'Maccio',
+      'cognome' : 'Capatonda',
+      'eta' : '40'
+    }
+  ];
+  
+  console.log(arrayStudenti);
+  
+  for (var i = 0; i < arrayStudenti.length; i++) {
+    console.log(arrayStudenti[i].nome + ' ' + arrayStudenti[i].cognome);
+  };
